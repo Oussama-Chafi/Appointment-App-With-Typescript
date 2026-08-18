f12cdfe86f3ef79be5de201c5ec7455db43544fe
Oussama <oussama.chafi.04@gmail.com>
2026-08-18 23:43:56 +0100

    refactor(error): enhance global error handler to handle Mongoose castError and duplicate keys

--- app-backend/src/middlewares/errorHandler.ts
+++ app-backend/src/middlewares/errorHandler.ts
@@ -3,6 +3,8 @@ import type { NextFunction, Request, Response } from "express";
 type ErrorType = Error & {
   statusCode: number;
   message: string;
+  name?: string;
+  code?: number;
 };
 const errorHandler = (
   err: ErrorType,
@@ -10,8 +12,16 @@ const errorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Interval Server Error";
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Interval Server Error";
+  if (err.name === "CastError") {
+    statusCode = 400;
+    message = "Invalid Resource ID format";
+  }
+  if (err.code === 11000) {
+    statusCode = 400;
+    message = "Duplicate field value entered.";
+  }
   res.status(statusCode).json({
     success: false,
     statusCode,
